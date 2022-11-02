518dedab8e84af320d4c8addfb4f720d3f108685
David Rodriguez <david.rodriguez@factorial.co>
2022-11-02 13:42:54 +0100

    peek definition for lsp saga

--- .config/nvim/after/plugin/lspsaga.lua
+++ .config/nvim/after/plugin/lspsaga.lua
@@ -5,5 +5,5 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gD', '<Cmd>Lspsaga lsp_finder<CR>', opts)
-vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
+vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', '<leader>r', '<Cmd>Lspsaga rename<CR>', opts)
