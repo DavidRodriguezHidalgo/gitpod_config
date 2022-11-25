aaaea340568c95aab50b3bb4617d58e238b7a0c8
David Rodriguez <denzilrh90@gmail.com>
2022-11-25 15:44:57 +0100

    auto install

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ cp -rf .dotfiles/.config ~
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+
+
+# Install packer dependencies
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' # Yep, twice
+nvim --headless -c 'TSUpdateSync'
