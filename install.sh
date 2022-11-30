d5d71876e7078494654b943c7729c7d9f6824fcb
David Rodríguez <denzilrh90@gmail.com>
2022-11-30 09:47:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,11 @@ cp -rf .dotfiles/.config ~
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-
+# Install npm for JS LSP
+sudo apt-get -y install npm
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
 
 # Install packer dependencies
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
